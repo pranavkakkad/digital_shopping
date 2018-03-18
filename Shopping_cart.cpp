ec7eff8225edcd6b4b7eeb8cc80041851a982f27
Nimesh <nimesh3536@gmail.com>
2018-03-18 22:19:40 +0530

    ADD aryan's contribution

---
+++ Shopping_cart.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class shopping_cart
+{
+	int day, mounth, year, p;
+
+public:
+	void creat_date()
+	{
+		cout << endl
+				 << "enter day" << endl;
+		cin >> day;
+		cout << endl
+				 << "enter mounth" << endl;
+		cin >> mounth;
+		cout << endl
+				 << "enter year" << endl;
+		cin >> year;
+	}
+	void saw_date()
+	{
+		cout << day << "/" << mounth << "/" << year << endl;
+	}
+	void total_product(int x)
+	{
+		p = x;
+	}
+	void saw_totalproduct()
+	{
+		cout << endl
+				 << p << endl;
+	}
+};
+class product
+{
+	string prdid;
+	string prdname, splrname;
+
+public:
+	string pruduct_name()
+	{
+		cout << endl
+				 << "enter product name" << endl;
+		cin >> prdname;
+		return prdname;
+	}
+	string product_id()
+	{
+		cout << endl
+				 << "enter product id" << endl;
+		cin >> prdid;
+		return prdid;
+	}
+	string supplier_name()
+	{
+		cout << endl
+				 << "enter product supplier name" << endl;
+		cin >> splrname;
+		return splrname;
+	}
+};
+
+int main()
+{
+	return 0;
+}
